2c033c280f2677a6201f05999f73adfce5f0b8eb
Nicolas Bourre <nbourrereg@hotmail.com>
2016-10-14 16:42:12 -0400

    Fin de la leçon 3

--- app/src/main/java/com/nicolasbourre/b76/meteobourre/DetailActivity.java
+++ app/src/main/java/com/nicolasbourre/b76/meteobourre/DetailActivity.java
@@ -1,26 +1,23 @@
 package com.nicolasbourre.b76.meteobourre;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.net.URL;
-
 public class DetailActivity extends AppCompatActivity {
 
-    private ShareActionProvider mShareActionProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +34,6 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
-
-        MenuItem item = menu.findItem(R.id.action_share);
-
-        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
 
         return true;
     }
@@ -52,13 +44,6 @@ public class DetailActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        if (id == R.id.action_share) {
-            String sharedContent = "";
-            setShareIntent(new Intent());
-
-            return true;
-        }
 
 
         return super.onOptionsItemSelected(item);
@@ -71,7 +56,13 @@ public class DetailActivity extends AppCompatActivity {
      */
     public static class DetailFragment extends Fragment {
 
+        private ShareActionProvider mShareActionProvider;
+        String mForecastStr;
+
+        private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -82,23 +73,48 @@ public class DetailActivity extends AppCompatActivity {
 
             Intent intent = getActivity().getIntent();
 
-
-
             if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
 
-                ((TextView) rootView.findViewById(R.id.detail_text)).setText(forecastStr);
+                ((TextView) rootView.findViewById(R.id.detail_text)).setText(mForecastStr);
             }
 
             return rootView;
         }
-    }
 
-    // Call to update the share intent
-    private void setShareIntent(Intent shareIntent) {
-        if (mShareActionProvider != null) {
-            mShareActionProvider.setShareIntent(shareIntent);
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detailfragment, menu);
+
+            MenuItem item = menu.findItem(R.id.action_share);
+
+            mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareForecastIntent());
+            } else {
+                Log.d (this.getClass().getSimpleName(), "Share action provider is null?" );
+            }
+
+        }
+
+        /**
+         * Permet de creer l'intent qui sera partage
+         * @return retourne l'intent a partager
+         */
+        private Intent createShareForecastIntent() {
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, mForecastStr + FORECAST_SHARE_HASHTAG);
+
+            intent.setType("text/plain");
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+
+            return intent;
         }
     }
+
+
 
 }
---
+++ app/src/main/java/com/nicolasbourre/b76/meteobourre/PowerConnectionReceiver.java
@@ -0,0 +1,17 @@
+package com.nicolasbourre.b76.meteobourre;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by nbourre on 2016-10-14.
+ */
+
+public class PowerConnectionReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Code pour le niveau de batterie
+    }
+}
